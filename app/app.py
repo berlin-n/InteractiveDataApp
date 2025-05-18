3c1af90853113db91ba782704a67eb7766e2382e
berlin-n <130678445+berlin-n@users.noreply.github.com>
2025-05-18 16:19:25 +0100

    First Iteration: I have done more documentation, things should be a lot more clearer now on the direction of the project. I did a little on the modelling tab, just added a functionality for model generation, the functionality I put in the home tab that waits for file upload will need work for every other tab (more explanation in documentation).

--- app/app.py
+++ app/app.py
@@ -3,11 +3,14 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression 
+from sklearn.tree import DecisionTreeRegressor
 
 st.set_page_config("Interactive Data App")
 st.title("Interactive Data App")
 
-tab0, tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(["Home", "Analyze", "Visualize", "Preprocess", "Feature Engineering", "Predict", "Dashboard"])
+tab0, tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(["Home", "Analyze", "Visualize", "Preprocess", "Feature Engineering", "Model Development", "Dashboard"])
 
 with tab0:
     st.title("Welcome to the Data Interaction App")
@@ -24,9 +27,13 @@ with tab0:
     """)
 
     st.markdown("---")
-    uploaded_file = st.file_uploader("Upload your CSV file")
-    df = pd.read_csv(uploaded_file)
-    st.dataframe(df.head())
+    uploaded_file = st.file_uploader("Upload your CSV file", type=["csv"])
+    if uploaded_file is not None:
+        df = pd.read_csv(uploaded_file)
+        st.success("Data upoaded successfully! Navigate to the tabs to begin.")
+        st.dataframe(df.head())
+    else:
+        st.info("Awaiting file upload...")
 
 with tab1:
     st.subheader("Analysis")
@@ -35,7 +42,6 @@ with tab1:
     st.write("Columns:", df.columns.to_list())
     st.write(df.describe())
     
-
 with tab2:
     st.subheader("Visualization")
 
@@ -46,20 +52,34 @@ with tab3:
         df.drop(columns=cols)
         st.write(df.head())
 
-
 with tab4:
     st.subheader("Feature Engineering")
     dependentVariable = st.selectbox("Select the column for your y-variable", df.columns)
     columns = st.multiselect("Select the columns for your X-variable", df.columns)
     y = df[dependentVariable]
-    x = df[columns]
+    features = df[columns]
 
 with tab5:
+    st.subheader("Modelling")
     models = ["Linear Regression", "Logistic Regression", "Multi Regression", "Decision Trees"]
-    st.subheader("Make predictions")
-    st.selectbox("Select a Model to use", models)
+    model = st.selectbox("Select a Model to use", models)
+    if model == "Linear Regression":
+        X = st.selectbox("Select your X variable", features)
+        linearRegressionModel = LinearRegression()
+        linearRegressionModel.fit(X,y)
+    elif model == "Multi Regression":
+        X = st.multiselect("Select your X variable", features)
+        linearRegressionModel = LinearRegression()
+        linearRegressionModel.fit(X,y)
+    elif model == "Logistic Regression":
+        X = st.multiselect("Select your X variable", features)
+        logisticRegressionModel = LogisticRegression()
+        logisticRegressionModel.fit(X,y)
+    elif model == "Decision Trees":
+        X = st.multiselect("Select your X variable", features)
+        decisionTreeRegressionModel = DecisionTreeRegressor()
+        decisionTreeRegressionModel.fit(X,y)
     
-
 with tab6:
     st.subheader("Generate a dashboard")
     
